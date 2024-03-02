61c96a0e879a3871944e2e8c66ccd78d606b6391
Pedro Jonas <pedrojonas.pj77@gmail.com>
2024-03-02 12:11:41 -0300

    feat: mostar e esconder form de criação

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+function showCreatFormBox () {
+    document.getElementById("addButton").style.display = "none";
+    document.getElementById("creatFormBox").style.display = "block";
+}
+
+function showCreatTask () {
+    document.getElementById("creatFormBox").style.display = "none";
+    document.getElementById("addButton").style.display = "block";
+}
